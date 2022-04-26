7a1f35dc4e6500516cfaf16cbcc5c4ab7abbe808
yangzhengca <yangzhengca@gmail.com>
2022-04-25 23:07:19 -0600

    finished lesson9

--- objects.js
+++ objects.js
@@ -1,38 +1,14 @@
-class User {
-  constructor(name, email){
-    this.name = name;
-    this.email = email;
-    this.score = 0;
-  }
-
-  login(){
-    console.log(this.name, 'just logged in');
-    return this;
-  }
-  logout(){
-    console.log(this.name, 'just logged out');
-    return this;
-  }
-
-  updateScore(){
-    this.score++;
-    console.log(this.name,"score is now", this.score);
-    return this;
+function User(name, email){
+  this.name = name;
+  this.email = email;
+  this.online = false;
+  this.login = function () {
+    console.log(this.email, 'has logged in')
   }
 }
-
-class Admin extends User {
-  deleteUser(user){
-    users = users.filter(u => u.email != user.email);
-  }
-}
-
 
 var userOne = new User('Ryu', 'ryu@gmail.com');
 var userTwo = new User('John', 'john@gmail.com');
-var admin = new Admin('Tom','tom@gmail.com');
 
-var users = [userOne, userTwo];
 
-admin.deleteUser(userTwo);
-console.log(users)
+console.log(userOne)

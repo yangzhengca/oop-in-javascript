43d4613a01375bb741b49b35fe76374450fddfc4
yangzhengca <yangzhengca>
2022-04-26 12:41:25 -0600

    finished lesson11

--- objects.js
+++ objects.js
@@ -17,6 +17,24 @@ User.prototype.logout = function(){
 
 }
 
+function Admin(...args){
+  // inheritance User
+  User.apply(this, args);
+
+  this.role = 'super admin';
+}
+
+// inheritance prototype from User
+Admin.prototype = Object.create(User.prototype);
+
+Admin.prototype.deleteUser = function(user){
+  users = users.filter( u => u.email != user.email);
+}
+
 var userOne = new User('Ryu', 'ryu@gmail.com');
 var userTwo = new User('John', 'john@gmail.com');
+var admin = new Admin('Tom', 'tom@gmail.com');
 
+var users = [userOne, userTwo, admin];
+
+console.log(admin)

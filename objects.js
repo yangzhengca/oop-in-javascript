4078a461a2e0c4d7c3d7e3dcee853b5861eaa371
yangzhengca <yangzhengca@gmail.com>
2022-04-25 22:33:58 -0600

    finished lesson7, method chain

--- objects.js
+++ objects.js
@@ -2,17 +2,30 @@ class User {
   constructor(name, email){
     this.name = name;
     this.email = email;
+    this.score = 0;
   }
 
   login(){
     console.log(this.name, 'just logged in');
+    return this;
   }
   logout(){
     console.log(this.name, 'just logged out');
+    return this;
   }
 
+  updateScore(){
+    this.score++;
+    console.log(this.name,"score is now", this.score);
+    return this;
+  }
 }
 
 var userOne = new User('Ryu', 'ryu@gmail.com');
+var userTwo = new User('John', 'john@gmail.com');
 
-userOne.login()
+userOne.login().updateScore().updateScore().logout()
+
+// userOne.updateScore().updateScore()
+
+// userTwo.updateScore()

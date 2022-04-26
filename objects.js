728dc0e8ebb88d9b1e9c20565c91c8574fef1bf8
yangzhengca <yangzhengca>
2022-04-26 12:09:45 -0600

    finished lesson10

--- objects.js
+++ objects.js
@@ -2,13 +2,21 @@ function User(name, email){
   this.name = name;
   this.email = email;
   this.online = false;
-  this.login = function () {
-    console.log(this.email, 'has logged in')
-  }
+
+}
+
+User.prototype.login = function(){
+    this.online = true;
+    console.log(this.email, 'has logged in');
+
+}
+
+User.prototype.logout = function(){
+  this.online = false;
+  console.log(this.email, 'has logged out');
+
 }
 
 var userOne = new User('Ryu', 'ryu@gmail.com');
 var userTwo = new User('John', 'john@gmail.com');
 
-
-console.log(userOne)
